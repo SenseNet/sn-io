bf97a8609f4e7453f73fd81c79e532f5c379a5be
kavics <gyeby5@gmail.com>
2021-07-21 17:47:41 +0200

    Basic project struct.

---
+++ src/SenseNet.IO.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SenseNet.IO.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/SenseNet.IO/IContent.cs
@@ -0,0 +1,14 @@
+﻿namespace SenseNet.IO
+{
+    public interface IContent
+    {
+        object this[string fieldName] { get; set; }
+
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Path { get; set; }
+        public string ParentPath { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
