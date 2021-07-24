8825d38f282f5de3ea25f1eeec948a35ee413a3b
kavics <gyeby5@gmail.com>
2021-07-24 09:37:48 +0200

    Dataflow proto.

--- src/SenseNet.IO.CLI/Program.cs
+++ src/SenseNet.IO.CLI/Program.cs
@@ -1,12 +1,96 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace SenseNet.IO.CLI
 {
     class Program
     {
-        static void Main(string[] args)
+        private static int[] _source = new int[500];
+        private static ConcurrentQueue<int> _target = new(new List<int>(200));
+
+        static async Task Produce(ITargetBlock<int> target)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < _source.Length; ++i)
+            {
+                await target.SendAsync(_source[i]);
+                var count = ((BufferBlock<int>)target).Count;
+                Console.Write($" {count}");
+            }
+            target.Complete();
+        }
+
+        static async Task<int> ConsumeAsync(ISourceBlock<int> source)
+        {
+            try
+            {
+                int processed = 0;
+                while (await source.OutputAvailableAsync())
+                {
+                    var data = await source.ReceiveAsync();
+                    _target.Enqueue(data);
+                    processed += 1;
+                    var count = ((BufferBlock<int>) source).Count;
+                    Console.Write($" {count}");
+                    await Task.Delay(1);
+                }
+
+                return processed;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        static async Task Main()
+        {
+            var rand = new Random();
+            for (int i = 0; i < _source.Length; i++)
+                _source[i] = rand.Next(100, 1000);
+
+            var options = new DataflowBlockOptions {BoundedCapacity = 10};
+            var buffer = new BufferBlock<int>(options);
+
+            var consumerTasks = new Task<int>[5];
+            for (int i = 0; i < consumerTasks.Length; i++)
+                consumerTasks[i] = ConsumeAsync(buffer);
+            var producerTasks = new Task[1];
+            for (int i = 0; i < producerTasks.Length; i++)
+                producerTasks[i] = Produce(buffer);
+
+            var allTasks = consumerTasks.Union(producerTasks).ToArray();
+
+            try
+            {
+                await Task.WhenAll(allTasks);
+            }
+            catch (Exception e)
+            {
+                // InvalidOperationException: The source completed without providing data to receive.
+                Console.WriteLine("ERROR");
+            }
+
+            var processed = -1;
+            try
+            {
+                processed = consumerTasks.Sum(x => x.Result);
+            }
+            catch (Exception e)
+            {
+                // InvalidOperationException: The source completed without providing data to receive.
+                Console.WriteLine("ERROR");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Processed {processed:#,#}.");
+            Console.WriteLine($"Source length: {_source.Length:#,#}.");
+            Console.WriteLine($"Target length: {_target.Count:#,#}.");
+            Console.WriteLine($"Source sum: {_source.Sum():#,#}.");
+            Console.WriteLine($"Target sum: {_target.Sum():#,#}.");
         }
     }
 }
