68d84ca706566ab62bce6e2470c5d1bf64c4978f
kavics <gyeby5@gmail.com>
2021-08-05 08:01:05 +0200

    Fix progress.

--- src/SenseNet.IO/ContentFlow.cs
+++ src/SenseNet.IO/ContentFlow.cs
@@ -25,16 +25,21 @@ namespace SenseNet.IO
                 if (Writer.RootName != null)
                     Rename(Reader.Content, rootName);
                 await Writer.WriteAsync(ContentPath.Combine(rootName, Reader.RelativePath), Reader.Content, cancel);
+                Progress(ref count, progress);
             }
             while (await Reader.ReadAsync(cancel))
             {
                 await Writer.WriteAsync(ContentPath.Combine(rootName, Reader.RelativePath), Reader.Content, cancel);
-                ++count;
-
-                var totalCount = Reader.EstimatedCount;
-                if(totalCount > 0)
-                    progress?.Report(count * 100.0 / totalCount);
+                Progress(ref count, progress);
             }
+        }
+
+        private void Progress(ref int count, IProgress<double> progress = null)
+        {
+            ++count;
+            var totalCount = Reader.EstimatedCount;
+            if (totalCount > 0)
+                progress?.Report(count * 100.0 / totalCount);
         }
 
         private void Rename(IContent content, string newName)
